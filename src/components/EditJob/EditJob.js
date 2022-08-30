55d9ec61173c052fddad945c6892a07b2a456f9a
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-08-30 12:36:20 +1200

    create Delete button and add Edit Job component

--- src/components/EditJob/EditJob.js
+++ src/components/EditJob/EditJob.js
@@ -1,3 +1,29 @@
+import { jobListState } from "../../state/atoms";
+import { v4 as uuid } from "uuid";
+import { useNavigate } from "react-router-dom";
+import { JobForm } from "../Common";
+import { useRecoilState } from "recoil";
+
 export const EditJob = () => {
-  return <div>Edit</div>;
+  const [jobList, setJobList] = useRecoilState(jobListState);
+  const navigate = useNavigate();
+  const handleSubmit = (values) => {
+    setJobList([...jobList, values]);
+    navigate("/");
+  };
+  return (
+    <JobForm
+      onJobSave={handleSubmit}
+      job={{
+        jobId: uuid(),
+        createdDate: Date.now(),
+        clientName: "",
+        clientEmail: "",
+        jobName: "",
+        clientPhoneNumber: null,
+        notes: "",
+        status: "",
+      }}
+    />
+  );
 };
--- src/components/JobList/JobItem.js
+++ src/components/JobList/JobItem.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { jobListState } from "../../state/atoms";
 import { Text, Group, Button, Container, Badge } from "@mantine/core";
 
+const removeItemAtIndex = (arr, index) => {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+};
+
 const JobItem = ({ item }) => {
+  const [jobList, setJobList] = useRecoilState(jobListState);
+  const index = jobList.findIndex((listItem) => listItem === item);
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(jobList, index);
+    setJobList(newList);
+  };
+
   return (
     <Container>
       <Group>
         <Text>{item.jobName}</Text>
         <Text>{item.clientName}</Text>
         <Badge>{item.status}</Badge>
-        <Button color="red">Delete</Button>
+        <Button color="red" onClick={deleteItem}>
+          Delete
+        </Button>
       </Group>
     </Container>
   );
