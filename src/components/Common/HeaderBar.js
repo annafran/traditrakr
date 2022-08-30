665fd956bc0f5957cf60cfefebf4b4fd2bbf45dd
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-08-30 21:47:31 +1200

    increase height headerBar

--- src/components/Common/HeaderBar.js
+++ src/components/Common/HeaderBar.js
@@ -1,140 +1,148 @@
 import React from "react";
 import {
-  createStyles,
-  Header,
-  Container,
-  Group,
-  Burger,
-  Paper,
-  Transition,
+    createStyles,
+    Header,
+    Container,
+    Group,
+    Burger,
+    Paper,
+    Transition,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { TraditrakrLogo } from "./TraditrakrLogo";
 import { useNavigate } from "react-router-dom";
 
-const HEADER_HEIGHT = "10vh";
+const HEADER_HEIGHT = "15vh";
 
 const useStyles = createStyles((theme) => ({
-  root: {
-    backgroundColor: "#F7B32B",
-    position: "relative",
-    zIndex: 1,
-  },
-
-  dropdown: {
-    position: "absolute",
-    top: HEADER_HEIGHT,
-    left: 0,
-    right: 0,
-    zIndex: 100,
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopWidth: 0,
-    overflow: "hidden",
-    backgroundColor: "#FCF6B1",
-
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  header: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "100%",
-  },
-
-  links: {
-    [theme.fn.smallerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "8px 12px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color: theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-    "&:hover": {
-      backgroundColor: theme.colors.gray[0],
+    root: {
+        backgroundColor: "#F7B32B",
+        position: "relative",
+        zIndex: 1,
     },
 
-    [theme.fn.smallerThan("sm")]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
-    },
-  },
+    dropdown: {
+        position: "absolute",
+        top: HEADER_HEIGHT,
+        left: 0,
+        right: 0,
+        zIndex: 100,
+        borderTopRightRadius: 0,
+        borderTopLeftRadius: 0,
+        borderTopWidth: 0,
+        overflow: "hidden",
+        backgroundColor: "#FCF6B1",
 
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.colors.gray[0],
+        [theme.fn.largerThan("sm")]: {
+            display: "none",
+        },
     },
-  },
+
+    header: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "100%",
+    },
+
+    links: {
+        [theme.fn.smallerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    burger: {
+        [theme.fn.largerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    link: {
+        display: "block",
+        lineHeight: 1,
+        padding: "8px 12px",
+        borderRadius: theme.radius.sm,
+        textDecoration: "none",
+        color: theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+        "&:hover": {
+            backgroundColor: theme.colors.gray[0],
+        },
+
+        [theme.fn.smallerThan("sm")]: {
+            borderRadius: 0,
+            padding: theme.spacing.md,
+        },
+    },
+
+    linkActive: {
+        "&, &:hover": {
+            backgroundColor: theme.colors.gray[0],
+        },
+    },
 }));
 
 export const HeaderBar = ({ active, setActive }) => {
-  const navigate = useNavigate();
-  const links = [
-    { link: "/", label: "Jobs" },
-    { link: "/createjob", label: "Create a job" },
-  ];
+    const navigate = useNavigate();
+    const links = [
+        { link: "/", label: "Jobs" },
+        { link: "/createjob", label: "Create a job" },
+    ];
 
-  const [opened, { toggle, close }] = useDisclosure(false);
-  // const [active, setActive] = useState(links[0].link);
-  const { classes, cx } = useStyles();
+    const [opened, { toggle, close }] = useDisclosure(false);
+    // const [active, setActive] = useState(links[0].link);
+    const { classes, cx } = useStyles();
 
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === link.link,
-      })}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-        close();
-        navigate(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
+    const items = links.map((link) => (
+        <a
+            key={link.label}
+            href={link.link}
+            className={cx(classes.link, {
+                [classes.linkActive]: active === link.link,
+            })}
+            onClick={(event) => {
+                event.preventDefault();
+                setActive(link.link);
+                close();
+                navigate(link.link);
+            }}
+        >
+            {link.label}
+        </a>
+    ));
 
-  return (
-    <Header height={HEADER_HEIGHT} className={classes.root}>
-      <Container className={classes.header}>
-        <TraditrakrLogo />
-        <Group spacing={5} className={classes.links}>
-          {items}
-        </Group>
+    return (
+        <Header height={HEADER_HEIGHT} className={classes.root}>
+            <Container className={classes.header}>
+                <TraditrakrLogo />
+                <Group spacing={5} className={classes.links}>
+                    {items}
+                </Group>
 
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-        />
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    className={classes.burger}
+                    size="sm"
+                />
 
-        <Transition transition="pop-top-right" duration={200} mounted={opened}>
-          {(styles) => (
-            <Paper className={classes.dropdown} withBorder style={styles}>
-              {items}
-            </Paper>
-          )}
-        </Transition>
-      </Container>
-    </Header>
-  );
+                <Transition
+                    transition="pop-top-right"
+                    duration={200}
+                    mounted={opened}
+                >
+                    {(styles) => (
+                        <Paper
+                            className={classes.dropdown}
+                            withBorder
+                            style={styles}
+                        >
+                            {items}
+                        </Paper>
+                    )}
+                </Transition>
+            </Container>
+        </Header>
+    );
 };
