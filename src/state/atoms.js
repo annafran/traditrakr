2dee964651c1cf889ddd9fdca46c3ced27431e3f
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-08-30 22:12:54 +1200

    fix sort and filter function

--- src/state/atoms.js
+++ src/state/atoms.js
@@ -1,13 +1,13 @@
 import { atom, selector } from "recoil";
 
 export const jobListState = atom({
-  key: "JobList",
-  default: [],
+    key: "JobList",
+    default: [],
 });
 
 export const jobListFilterState = atom({
-  key: "JobListFilter",
-  default: { status: "Show All", sort: "Most Recent" },
+    key: "JobListFilter",
+    default: { status: "Show All", sort: "Most Recent" },
 });
 
 // export const jobListSortState = atom({
@@ -16,47 +16,48 @@ export const jobListFilterState = atom({
 // });
 
 const filterItems = (filter, jobList) => {
-  switch (filter) {
-    case "Show Completed":
-      return jobList.filter((item) => item.status === "completed");
-    case "Show Uncompleted":
-      return jobList.filter((item) => item.status !== "completed");
-    case "Show Invoicing":
-      return jobList.filter((item) => item.status === "invoicing");
-    case "Show Priced":
-      return jobList.filter((item) => item.status === "priced");
-    case "Show Scheduled":
-      return jobList.filter((item) => item.status === "scheduled");
-    case "Show Active":
-      return jobList.filter((item) => item.status === "active");
-    default:
-      return jobList;
-  }
+    switch (filter) {
+        case "Show Completed":
+            return jobList.filter((item) => item.status === "completed");
+        case "Show Uncompleted":
+            return jobList.filter((item) => item.status !== "completed");
+        case "Show Invoicing":
+            return jobList.filter((item) => item.status === "invoicing");
+        case "Show Priced":
+            return jobList.filter((item) => item.status === "priced");
+        case "Show Scheduled":
+            return jobList.filter((item) => item.status === "scheduled");
+        case "Show Active":
+            return jobList.filter((item) => item.status === "active");
+        default:
+            return jobList;
+    }
 };
 
 const sortItems = (sort, jobList) => {
-  switch (sort) {
-    case "Most Recent":
-      return jobList.sort(
-        (itema, itemb) => itema.createdDate - itemb.createdDate
-      );
-    case "Least Recent":
-      return jobList.sort(
-        (itema, itemb) => itemb.createdDate - itema.createdDate
-      );
-    default:
-      return jobList;
-  }
+    switch (sort) {
+        case "Most Recent":
+            return jobList.sort(
+                (itema, itemb) => itemb.createdDate - itema.createdDate
+            );
+        case "Least Recent":
+            return jobList.sort(
+                (itema, itemb) => itema.createdDate - itemb.createdDate
+            );
+        default:
+            return jobList;
+    }
 };
 
 export const filteredJobListState = selector({
-  key: "filteredJobList",
-  get: ({ get }) => {
-    const { status, sort } = get(jobListFilterState);
-    const jobList = get(jobListState);
-    const filterList = filterItems(status, jobList);
-    return sortItems(sort, filterList);
-  },
+    key: "filteredJobList",
+    get: ({ get }) => {
+        const { status, sort } = get(jobListFilterState);
+        const jobList = get(jobListState);
+        const filterList = filterItems(status, jobList);
+        return sortItems(sort, filterList.slice());
+    },
+    dangerouslyAllowMutability: true,
 });
 
 // export const sortedJobListState = selector({
